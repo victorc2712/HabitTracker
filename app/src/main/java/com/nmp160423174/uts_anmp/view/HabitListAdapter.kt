017b37180d3d8492c70e29f065dac59fbb38fa66
Xaputtt <s160423173@student.ubaya.ac.id>
2026-05-05 12:46:33 +0700

    HabitListAdapter if else agar button tidak bug

--- app/src/main/java/com/nmp160423174/uts_anmp/view/HabitListAdapter.kt
+++ app/src/main/java/com/nmp160423174/uts_anmp/view/HabitListAdapter.kt
@@ -2,6 +2,7 @@ package com.nmp160423174.uts_anmp.view
 
 import android.R
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.nmp160423174.uts_anmp.databinding.HabitCardItemBinding
@@ -23,12 +24,43 @@ class HabitListAdapter(val habitList: ArrayList<Habit>): RecyclerView.Adapter<Ha
         holder.binding.txtHabit.text = habitList[position].name
         holder.binding.txtDesc.text = habitList[position].description
         holder.binding.txtProgress.text = habitList[position].progress.toString() + "/"+habitList[position].goal
+        holder.binding.progressBar.progress= habitList[position].progress
+        holder.binding.progressBar.max= habitList[position].goal
 
-//        holder.binding.btnDetail.setOnClickListener {
-//            val student = habitList[position]
-//            val action = HabitListFragmentDirections.actionStudentDetailFragment(student)
-//            it.findNavController().navigate(action)
-//        }
+        if(habitList[position].progress < habitList[position].goal)
+        {
+            holder.binding.progressView.visibility = View.GONE
+            holder.binding.chipComplete.visibility = View.GONE
+            holder.binding.chipProgress.text= "In Progress"
+            holder.binding.btnPlus.isEnabled=true
+        }
+        else
+        {
+            holder.binding.progressView.visibility = View.VISIBLE
+            holder.binding.chipComplete.visibility = View.VISIBLE
+            holder.binding.btnPlus.isEnabled=false
+            holder.binding.chipProgress.text="Complete"
+           // holder.binding.chipProgress.chipBackgroundColor
+
+        }
+        if (habitList[position].progress==0)
+        {
+            holder.binding.btnMinus.isEnabled=false
+        }
+        else
+        {
+            holder.binding.btnMinus.isEnabled=true
+        }
+        holder.binding.btnPlus.setOnClickListener {
+            habitList[position].progress++
+            notifyDataSetChanged()
+        }
+        holder.binding.btnMinus.setOnClickListener {
+            habitList[position].progress--
+            notifyDataSetChanged()
+        }
+
+
     }
 
 
